0b18b79f76c5f4773f01c23c52c8b3924940da8c
redcliphaloe <redcliphaloe@gmail.com>
2019-09-25 14:40:42 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function zeros(expression) {
   // your solution
+  let numOfZeros = 0;
+  let multipliers = expression.split('*');
+  let isContainTwo = false;
+  for (let i = 0; i < multipliers.length; i++) {
+    let factorialBase = parseInt(multipliers[i], 10);  
+    let dec = multipliers[i].split('!').length - 1;
+    if (!isContainTwo && (dec === 1 || factorialBase % 2 === 0)) {
+      isContainTwo = true;
+    }  
+
+    for (let j = factorialBase; j >= 5; j -= dec) {
+      if (j % 25 === 0) {
+        numOfZeros++;
+      }
+      if (j % 5 === 0) {
+        numOfZeros++; 
+      }      
+    }   
+  }
+  if (!isContainTwo) {
+    numOfZeros = 0;
+  }  
+    
+  return numOfZeros;  
 }
